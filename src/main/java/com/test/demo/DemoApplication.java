da48a9b7106be15b69b1195695432dd98d1c6cc1
torla <marcotorlaschi95@gmail.com>
2019-03-25 11:29:31 +0100

    dependency

--- src/main/java/com/test/demo/DemoApplication.java
+++ src/main/java/com/test/demo/DemoApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class DemoApplication {
 
   public static void main (String[] args) {
+    TimeZone.setDefault(TimeZone.getTimeZone("IST"));
     ConfigurableApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
   }
 
